5d787e1c3cc788926ad1a9fbf97a111566646b67
Thomas <12449668+indifferentghost@users.noreply.github.com>
2023-12-14 13:56:12 -0700

    Add kysley code generation

--- lib/env.ts
+++ lib/env.ts
@@ -1,10 +1,19 @@
 import Bun from "bun";
-import { object, parse, picklist } from "valibot";
+import {
+	url,
+	minLength,
+	object,
+	parse,
+	picklist,
+	string,
+	toTrimmed,
+} from "valibot";
 
 const ENV_LIST = ["development", "production"] as const;
 
 const EnvironmentSchema = object({
 	NODE_ENV: picklist(ENV_LIST, `ENV isn't in list ${ENV_LIST.join(" | ")}`),
+	DATABASE_URL: string([toTrimmed(), minLength(1), url()]),
 });
 
 export const env = parse(EnvironmentSchema, Bun.env);
--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,10 @@
 import { LibsqlDialect } from "@libsql/kysely-libsql";
 import { Kysely } from "kysely";
-import { Database } from "./types";
+import { DB as Database } from "kysely-codegen";
+import { env } from "../lib/env";
 
 export const db = new Kysely<Database>({
 	dialect: new LibsqlDialect({
-		url: "libsql://localhost:8080?tls=0", // I think it will be easier to set up local first
-		authToken: "<token>", // optional
+		url: env.DATABASE_URL,
 	}),
 });
