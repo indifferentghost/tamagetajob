5669660d0ca2cedc74ff5dbf1eadf543abdb0345
Thomas <12449668+indifferentghost@users.noreply.github.com>
2023-12-14 12:36:02 -0700

    format and lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import { Hono } from "hono";
-import { jsx } from 'hono/jsx';
+import { jsx } from "hono/jsx";
+import "../lib/env";
 import { Layout } from "./components/layout";
-import '../lib/env';
 
 const app = new Hono();
 
 app.get("/", (c) =>
-  c.html(
-    <Layout title="Home">
-      <h1 class="h1">hello</h1>
-    </Layout>
-  )
+	c.html(
+		<Layout title="Home">
+			<h1 class="h1">hello</h1>
+		</Layout>,
+	),
 );
 
 export default app;
