d19e9ff29e94a5d6385f95d3a8241a9e817783cb
Thomas <12449668+indifferentghost@users.noreply.github.com>
2023-12-14 18:54:52 -0700

    More properly allow DATABASE_URL between environments
    Based on comment https://github.com/indifferentghost/tamagetajob/pull/4/files#r1427433901

--- lib/env.ts
+++ lib/env.ts
@@ -13,25 +13,29 @@ import {
 	union,
 } from "valibot";
 
+const UrlSchema = [
+	toTrimmed(),
+	minLength<string, 1>(1, "URL cannot be an empty string"),
+	url("URL must match a url format"), // The URL shouldn't error in the URL constructor `new URL`
+	toLowerCase(),
+] as const;
+
 const DevEnvSchema = object({
 	NODE_ENV: literal("development"),
-	DATABASE_URL: string([
-		toTrimmed(),
-		toLowerCase(),
-		minLength(1),
-		url(),
-		endsWith("?tls=0"),
+	DATABASE_URL: string("", [
+		...UrlSchema,
+		endsWith(
+			"?tls=0",
+			"development DATABASE_URL doesn't end in ?tls=0 and probably should.",
+		),
 	]),
 });
 
 const ProdEnvSchema = object({
 	NODE_ENV: literal("production"),
 	DATABASE_URL: string([
-		toTrimmed(),
-		toLowerCase(),
-		minLength(1),
-		url(),
-		excludes("?tls=0"),
+		...UrlSchema,
+		excludes("?tls=0", "production DATABASE_URL running without https."),
 	]),
 });
 
